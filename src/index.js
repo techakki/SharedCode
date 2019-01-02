d09377dc8ad770007df2bbca00dbf18e9be89e9e
techakki <akshaykmr869@gmail.com>
2019-01-02 12:06:06 +0530

    refactor code

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import reducer from './src/state_management/reducers';
+import reducer from './state_management/reducers';
 import {
     addPlace,
     removePlace,
     removeAll
-} from './src/state_management/actions';
+} from './state_management/actions';
 
 const actions = {
     addPlace,
